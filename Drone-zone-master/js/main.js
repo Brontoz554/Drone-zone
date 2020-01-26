c6da32e1901f1964a64f7f7ef25ca646ac8378e7
Николай <47238111+Brontoz554@users.noreply.github.com>
2020-01-26 09:00:58 +0300

    Update main.js

--- Drone-zone-master/js/main.js
+++ Drone-zone-master/js/main.js
@@ -1,14 +1,16 @@
-// setTimeout(() => {
-//     document.querySelector('body').style.overflowY = 'hidden';
-//     setTimeout(() => {
-//         document.querySelector('.modal').style.display = 'flex';
-//         document.querySelector('.modal').style.top = window.pageYOffset + 'px';
-//     }, 100)
-// }, 1000);
-//
-//
-// function hiddenModal()
-// {
-//     document.querySelector('body').style.overflowY = 'auto';
-//     document.querySelector('.modal').style.display = 'none';
-// }
+setTimeout(() => {
+    document.querySelector('body').style.overflowY = 'hidden';
+    setTimeout(() => {
+        document.querySelector('.modal').style.display = 'block';
+        document.querySelector('.modal').style.position = 'fixed';
+        document.querySelector('.modal').style.top = 0;
+        document.querySelector('.modal').style.left = 0;
+    }, 100)
+}, 1000);
+
+
+function hiddenModal()
+{
+    document.querySelector('body').style.overflowY = 'auto';
+    document.querySelector('.modal').style.display = 'none';
+}
