fbe0303e597b39ae6eb5042a04ee4081272642c1
HaterWordPress <tifilum74@mail.ru>
2020-01-26 12:47:00 +0300

    maket

--- Drone-zone-master/js/main.js
+++ Drone-zone-master/js/main.js
@@ -1,15 +1,14 @@
 // setTimeout(() => {
-// //     document.querySelector('body').style.overflowY = 'hidden';
-// //     setTimeout(() => {
-// //         document.querySelector('.modal').style.display = 'flex';
-// //         document.querySelector('.modal').style.top = window.pageYOffset + 'px';
-// //     }, 100)
-// // }, 1000);
-// //
-// //
-// // function hiddenModal()
-// // {
-// //     document.querySelector('body').style.overflowY = 'auto';
-// //     document.querySelector('.modal').style.display = 'none';
-// //
-// // }
+//     document.querySelector('body').style.overflowY = 'hidden';
+//     setTimeout(() => {
+//         document.querySelector('.modal').style.display = 'flex';
+//         document.querySelector('.modal').style.top = window.pageYOffset + 'px';
+//     }, 100)
+// }, 1000);
+//
+//
+// function hiddenModal()
+// {
+//     document.querySelector('body').style.overflowY = 'auto';
+//     document.querySelector('.modal').style.display = 'none';
+// }
